b3f2bbfd5190c3f861b85451fda91af09f8a9966
Ali Sadeghi <ali909392@gmail.com>
2023-08-30 21:20:44 +0330

    Spinning wheel lib works

---
+++ Task-Assigner/script.js
@@ -0,0 +1,12 @@
+
+libSegments = [
+    { fillStyle: "#eae56f", text: "Prize 1" },
+    { fillStyle: "#89f26e", text: "Prize 2" },
+    { fillStyle: "#7de6ef", text: "Prize 3" },
+    { fillStyle: "#e7706f", text: "Prize 4" },
+    { fillStyle: "#eae56f", text: "Prize 5" },
+    { fillStyle: "#89f26e", text: "Prize 6" },
+    { fillStyle: "#7de6ef", text: "Prize 7" },
+    { fillStyle: "#e7706f", text: "Prize 8" },
+    
+];
